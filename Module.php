1840aa8e95898fed29d68916b99c194316e73e22
Huynh Tu Vinh <huynhtuvinh87@gmail.com>
2016-04-08 22:23:37 -0700

    blog

---
+++ Module.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace huynhtuvinh87\blog;
+use Yii;
+
+/**
+ * user module definition class
+ */
+class Module extends \yii\base\Module {
+
+    /**
+     * @inheritdoc
+     */
+    public $controllerNamespace = 'huynhtuvinh87\controllers\frontend';
+    /**
+     * @inheritdoc
+     */
+    public function init() {
+        parent::init();
+
+        // custom initialization code goes here
+    }
+
+}
---
+++ controllers/frontend/DefaultController.php
@@ -0,0 +1,15 @@
+<?php
+namespace huynhtuvinh87\blog\controllers\frontend;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+use Yii;
+use yii\data\Pagination;
+use yii\web\Controller;
+
+class DefaultController extends Controller{
+    
+}
